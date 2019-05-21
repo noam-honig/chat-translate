f8c1d511e928e1cd3a2aae6f8a49dd2e5a38494f
Noam <noam@fireflymigration.com>
2019-05-21 17:44:44 +0300

    First draft of comunication is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.refreshEventListener(true);
+  }
+  constructor(private zone: NgZone, private http: HttpClient) {
+
+  }
+  text:string;
+  async textChanging(){
+    
+    await this.http.post('/api/test',{text:this.text}).toPromise();
+  }
+  otherText:string;
+
   title = 'chat-translate';
+  eventSource: any;/*EventSource*/
+  refreshEventListener(enable: boolean) {
+
+    if (typeof (window) !== 'undefined') {
+      let EventSource: any = window['EventSource'];
+      if (enable && typeof (EventSource) !== "undefined") {
+        this.zone.run(() => {
+          console.log('registering to stream');
+          var source = new EventSource('/api/stream', { withCredentials: true });
+          if (this.eventSource) {
+            this.eventSource.close();
+            this.eventSource = undefined;
+          }
+          this.eventSource = source;
+          source.onmessage = e => {
+            
+            this.zone.run(() => {
+              this.otherText = e.data.toString();
+              console.log(this.otherText);
+            });
+          };
+          let x = this;
+          source.addEventListener("authenticate", async function (e) {
+            x.http.post('/api/authenticate', { key: ((<any>e).data.toString()) }).toPromise().then(()=>{});
+
+          });
+        });
+      }
+    }
+  }
 }
--- src/app/server/server.ts
+++ src/app/server/server.ts
@@ -1,21 +1,63 @@
 import * as express from 'express';
+import { Express, Response } from 'express';
 import * as compression from 'compression';
 import * as fs from 'fs';
 import * as secure from 'express-force-https';
 import { config } from 'dotenv';
 import '../../stam';
+import * as bodyParser from 'body-parser';
 
 config();
 let app = express();
 let port = process.env.PORT || 3000;
-app.use(compression());
+
 if (!process.env.DISABLE_HTTPS)
     app.use(secure);
 
 
 app.use(express.static('dist'));
-app.get('/test',(req,res)=>{
-    res.json({item:'123'});
+app.use(bodyParser.json())
+
+let connection: Response[] = [];
+let tempConnections: any = {};
+app.get('/api/stream', (req, res) => {
+
+    res.writeHead(200, {
+        
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+    let key = new Date().toISOString();
+    
+    tempConnections[key] = () => {
+        connection.push(res);
+        tempConnections[key] = undefined;
+
+    };
+    console.log('registered to stream '+key);
+    res.write("event:authenticate\ndata:" + key + "\n\n");
+
+    req.on("close", () => {
+        tempConnections[key] = undefined;
+        let i = connection.indexOf(res);
+        if (i >= 0)
+            connection.splice(i, 1);
+    });
 });
+app.use(compression());
+app.post('/api/authenticate', (req, res) => {
+    let x = tempConnections[req.body.key];
+    if (x) {
+        x();
+        res.json({ok:true});
+    }
+});
+app.post('/api/test', (req, res) => {
+    let x = req.body.text;
+    connection.forEach(y => y.write("data:" + x + "\n\n"))
+    res.json({ item: '123' });
+});
+
 app.listen(port);
-console.log("Listening on port: "+port);
+console.log("Listening on port: " + port);
